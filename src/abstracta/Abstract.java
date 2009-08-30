443e5f5d3bac2c3216b6abad395173c4eed1f8d5
Jeroen Lapre <jeroen.lapre@gmail.com>
2009-08-29 21:50:25 -0700

    My very first git commit

---
+++ src/abstracta/Abstract.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package abstracta;
+
+/**
+ * @author Jeroen Lapre'
+ *
+ */
+public class Abstract {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
